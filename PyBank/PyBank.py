43134fb1aaa6e0e3dd607f2701e02d54797234ce
brianroberts778 <70656160+brianroberts778@users.noreply.github.com>
2020-10-09 16:07:31 -0400

    PyBank // PyPoll
    Went back to the beginning of PyBank to create lists and variables that were necessary to properly calculate the Profit/Loss from each month. Originally I didn't realize that the column as a whole needed to be recalculated.    For PyPoll, I retooled the variables/lists to make it easier to determine the total votes and percentages for rach candidate. Then,  conditionals were placed to append the total votes into candidate-specific lists and to determine the overall winner.

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -15,36 +15,57 @@ with open(pybank_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
 
+    # Variable for Profit and Loss
+    pl_change = []
+    pl_new = []
+    months = []
+    
+
     # Read the header row first
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
+    num_rows = -1
+
     # Read each row of data after the header
     for row in csvreader:
-        print(row)
-
-# Count the total number of months included in the dataset (be sure not to include header row!)
-num_rows = -1
-for row in open("pybank_Resources_budget_data.csv"):
+        
+        #
         num_rows += 1
+# Count the total number of months included in the dataset (be sure not to include header row!)
+        
+#or row in open("pybank_Resources_budget_data.csv"):
+        #num_rows += 1
+        # Appending colum 1
+        months.append(row[0])
+# Calculate the Profits/Losses per month 
+        pl_change.append(int(row[1]))
 
 # Calculate the net total amount of "Profit/Losses" for entire dataset
-with open("pybank_Resources_budget_data.csv") as csv_file:
-    reader = csv.DictReader(csv_file)
-    total = sum(float(row["Profit/Losses"]) for row in reader)
 
+    #total = sum(float(row["Profit/Losses"]) for row in csvreader)
+    total  = sum(pl_change)
+    total_months = len(months)
+
+
+    
+    for row in range(1,len(pl_change)):
+        pl_new.append(int(pl_change[row])-(int(pl_change[row-1]))) 
+        #print(pl_new)
     # Calculate the average change of the "Profit/Losses" column
-    avg_pl = round((total/num_rows),2)
+    #avg_pl = round((total/num_rows),2)
+    avg_pl = sum(pl_new)/len(pl_new)
 
 # Determine the greatest increase in profits (date and amount) over the entire period
-with open("pybank_Resources_budget_data.csv") as csv_file:
-    reader = csv.DictReader(csv_file)
-    max_pl = max(float(row["Profit/Losses"]) for row in reader)
+#with open("pybank_Resources_budget_data.csv") as csv_file:
+    #reader = csv.DictReader(csv_file)
+    #max_pl = max(float(row["Profit/Losses"]) for row in csvreader)
+    max_pl = max(pl_new)
+    min_pl = min(pl_new)
 
-# Determine the greatest decrease in losses (date and amount) over the entire period
-with open("pybank_Resources_budget_data.csv") as csv_file:
-    reader = csv.DictReader(csv_file)
-    min_pl = min(float(row["Profit/Losses"]) for row in reader)
+# Determine the greatest decrease in losses (date) over the entire period
+    max_month = months[pl_new.index(max_pl) + 1]
+    min_month = months[pl_new.index(min_pl) + 1]
 
 # Print analysis table (with all calculate varaibles from above)
 print("")
@@ -52,6 +73,6 @@ print('Financial Anlaysis')
 print('-----------------------------------')
 print('Total Months: ',num_rows) 
 print("Total: $" + format(str(total))+"0")
-print("Average Change: $" + str(avg_pl))
-print("Greatest Increase in Profits: $" + str(max_pl) + "0")
-print("Greatest Decrease in Losses: $" + str(min_pl) + "0")
+print("Average Change: $" + str(round(avg_pl,2)))
+print("Greatest Increase in Profits: " + str(max_month) + " $" +str(max_pl))
+print(f"Greatest Decrease in Losses: {str(min_month)}  (${str(min_pl)})")
--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -2,21 +2,81 @@
 import os
 import csv
 
+# Define Variables for later use 
+csv_votes = []
+csv_candidates = []
+khan = []
+correy = []
+li = []
+otooley = []
+
+
 # Create path to read provided CSV file
 pypoll_csv = os.path.join('..','PyPoll', 'PyPoll_election_data.csv')
 
-# Read using the CSV module
+
+# Read file using the CSV module
 with open(pypoll_csv) as csvfile:
 
     # Specify delimiter and variable to hold contents
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
+    #print(csvreader)
 
     # Read the header row first
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-    # Read each row of data after the header
-    for row in csvreader:
-        print(row)
+    # Append columns (Omitted 'County' because it does not apply to our analysis)
+    for column in csvreader:
+        csv_votes.append(column[0])
+        csv_candidates.append(column[2])
+
+    # Calculate the total number of votes cast
+    All_Votes = len(csv_votes)
+
+    for candidate in csv_candidates:
+
+        if candidate == "Khan":
+            khan.append(csv_candidates)
+            khan_votes = len(khan)
+
+        elif candidate == "Correy":
+            correy.append(csv_candidates)
+            correy_votes = len(correy)
+
+        elif candidate == "Li":
+            li.append(csv_candidates)
+            li_votes = len(li)
+
+        else:
+            otooley.append(csv_candidates)
+            otooley_votes = len(otooley)
+
+    # Percent
+    khan_percent = (khan_votes)/(All_Votes)
+    correy_percent = (correy_votes)/(All_Votes)
+    li_percent = (li_votes)/(All_Votes)
+    otooley_percent = (otooley_votes)/(All_Votes)
+
+    if khan_percent > max(correy_percent, li_percent, otooley_percent):
+        winner = "Khan"
+    elif correy_percent > max(khan_percent, li_percent, otooley_percent):
+        winner = "Correy"
+    elif li_percent > max(khan_percent, correy_percent, otooley_percent):
+        winner = "li"
+    else: 
+        winner = "otooley"
+
+        # Determine the total numeber of votes received by each candidate
+        # khan_total = csv_candidates.count("Khan")
+
+    
         
+
+
+# Print Analysis Table
+print("Election Results")
+print("------------------------------")
+print("Total Votes: " + str(All_Votes))
+print("------------------------------")
+print(khan_votes, otooley_percent, winner)
